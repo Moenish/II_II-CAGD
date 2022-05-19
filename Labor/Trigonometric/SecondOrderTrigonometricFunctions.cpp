c49a88b79e3ebef24b55c007a7b0a5ad198fdf9f
Moenish <krisztiatilla@yahoo.com>
2022-05-19 18:36:26 +0300

    Refix shit

--- Labor/Trigonometric/SecondOrderTrigonometricFunctions.cpp
+++ Labor/Trigonometric/SecondOrderTrigonometricFunctions.cpp
@@ -48,10 +48,11 @@ GLdouble SOTF::f2_1(GLdouble alpha, GLdouble t)
     GLdouble c3 = c1 - c2;
 
     GLdouble sin4Alpha = sin(c1) * sin(c1) * sin(c1) * sin(c1);
-    GLdouble cos2Alpha = cos(c1) * cos(c1);
+//    GLdouble cos2Alpha = cos(c1) * cos(c1);
 
-    return (sin(c3) * cos(c2) * sin(c2) * (6.0 * cos(c1) * sin(c2) + (2.0 * cos2Alpha + 1.0) * sin(c3)))
-            / sin4Alpha;
+//    return (sin(c3) * cos(c2) * sin(c2) * (6.0 * cos(c1) * sin(c2) + (2.0 * cos2Alpha + 1.0) * sin(c3))) / sin4Alpha;
+    return -(sin(c2)*(2.0*cos(c1)*cos(c3)*sin(c2)*sin(c2)+((2.0*cos(c1)*cos(c1)+1.0)*cos(c3)*sin(c3)-6*cos(c1)*sin(c3)*cos(c2))*sin(c2)+(-2.0*cos(c1)*cos(c1)-1.0)*sin(c3)*sin(c3)*cos(c2)))/sin4Alpha;
+
 }
 GLdouble SOTF::f3_1(GLdouble alpha, GLdouble t)
 {
@@ -61,7 +62,8 @@ GLdouble SOTF::f3_1(GLdouble alpha, GLdouble t)
     GLdouble sin4Alpha = sin(c1) * sin(c1) * sin(c1) * sin(c1);
     GLdouble sin3T = sin(c2) * sin(c2) * sin(c2);
 
-    return (2.0 * cos(c2) * sin3T) / sin4Alpha;
+//    return (2.0 * cos(c2) * sin3T) / sin4Alpha;
+    return (1.0 / (sin4Alpha)) * 2.0 * (sin3T) * cos(t / 2.0);
 }
 
 GLdouble SOTF::f0_2(GLdouble alpha, GLdouble t)
@@ -83,7 +85,8 @@ GLdouble SOTF::f2_2(GLdouble alpha, GLdouble t)
     GLdouble sin2T = sin(c2) * sin(c2);
     GLdouble sin3T = sin2T * sin(c2);
 
-    return -(sin(c3) * (6.0 * cos(c1) * sin3T + (2.0 * cos2Alpha + 1.0) * sin(c3) * sin2T -12.0 * cos(c1) * cos(c2) * cos(c2) * sin(c2) + (-2.0 * cos2Alpha - 1.0) * sin(c3) * cos(c2) * cos(c2))) / (2*sin4Alpha);
+//    return -(sin(c3) * (6.0 * cos(c1) * sin3T + (2.0 * cos2Alpha + 1.0) * sin(c3) * sin2T -12.0 * cos(c1) * cos(c2) * cos(c2) * sin(c2) + (-2.0 * cos2Alpha - 1.0) * sin(c3) * cos(c2) * cos(c2))) / (2*sin4Alpha);
+    return -(8.0*cos(alpha/2.0)*sin((alpha-t)/2.0)*sin3T+(12.0*cos(alpha/2.0)*cos((alpha-t)/2.0)*cos(t/2.0)+(4.0*cos(alpha/2.0)*cos(alpha/2.0)+2.0)*sin((alpha-t)/2.0)*sin((alpha-t)/2.0)+(-2.0*cos(alpha/2.0)*cos(alpha/2.0)-1.0)*cos((alpha-t)/2.0)*cos((alpha-t)/2.0))*sin(t/2.0)*sin(t/2.0)+((8.0*cos(alpha/2.0)*cos(alpha/2.0)+4.0)*cos((alpha-t)/2.0)*sin((alpha-t)/2.0)*cos(t/2.0)-12.0*cos(alpha/2.0)*sin((alpha-t)/2.0)*cos(t/2.0)*cos(t/2.0))*sin(t/2.0)+(-2.0*cos(alpha/2.0)*cos(alpha/2.0)-1.0)*sin((alpha-t)/2.0)*sin((alpha-t)/2.0)*cos(t/2.0)*cos(t/2.0))/(2.0*sin4Alpha);
 }
 GLdouble SOTF::f3_2(GLdouble alpha, GLdouble t)
 {
@@ -95,5 +98,6 @@ GLdouble SOTF::f3_2(GLdouble alpha, GLdouble t)
     GLdouble sin3T = sin2T * sin(c2);
     GLdouble sin4T = sin3T * sin(c2);
 
-    return -(sin4T - 3.0 * cos(c2) * cos(c2) * sin2T) / sin4Alpha;
+//    return -(sin4T - 3.0 * cos(c2) * cos(c2) * sin2T) / sin4Alpha;
+    return (1.0 / (sin4Alpha)) * 3.0 * (sin(t / 2.0) * sin(t / 2.0)) * cos(t / 2.0) * cos(t / 2.0) + (1.0 / (sin4Alpha)) * (sin3T) * -sin(t / 2.0);
 }
